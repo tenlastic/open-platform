8158ea22b80d82f91c9f316e7ea728ddb6bcf138
Dan Appello <appellod@gmail.com>
2019-08-30 16:46:33 -0400

    log error in e2e test

--- projects/nodejs/modules/web-server/src/middleware/error/error.middleware.ts
+++ projects/nodejs/modules/web-server/src/middleware/error/error.middleware.ts
@@ -9,6 +9,7 @@ export async function errorMiddleware(ctx: Context, next: MiddlewareCallback) {
   } catch (e) {
     const status = e.status || 400;
 
+    console.error(e);
     switch (e.name) {
       case 'PermissionError':
         ctx.response.status = 401;
