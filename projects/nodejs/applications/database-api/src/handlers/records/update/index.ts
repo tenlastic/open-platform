5f7b3d063ae54980aa9105b576e23a74b7d69556
Dan Appello <appellod@gmail.com>
2019-08-31 15:43:56 -0400

    suppress mochawesome messages, fix records tests

--- projects/nodejs/applications/database-api/src/handlers/records/update/index.ts
+++ projects/nodejs/applications/database-api/src/handlers/records/update/index.ts
@@ -15,7 +15,7 @@ export async function handler(ctx: Context) {
   const Permissions = new MongoosePermissions<RecordDocument>(Model, collection.permissions);
 
   const query = { _id: id, collectionId, databaseId };
-  const where = Permissions.where(query, ctx.state.user);
+  const where = await Permissions.where(query, ctx.state.user);
   const record = await Model.findOne(where).populate(Permissions.populateOptions);
 
   if (!record) {
