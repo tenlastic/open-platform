33708c3a9d7a61771051fc91a52011076561540e
appellod <appellod@gmail.com>
2020-04-07 18:05:21 -0400

    fix ci bootstrapping

--- .argo/continuous-integration-nodejs.sh
+++ .argo/continuous-integration-nodejs.sh
@@ -9,10 +9,10 @@ git reset --hard
 # Run Javascript Applications.
 cd "${ROOT}/projects/javascript/"
 npm i -g lerna
-lerna bootstrap --ci --include-dependencies --ignore @tenlastic/*-ui --ignore @tenlastic/ng-* --scope @tenlastic/* --since
-lerna run --ci --ignore @tenlastic/*-ui --ignore @tenlastic/ng-* --scope @tenlastic/* --scope nodejs lint
-lerna run --ci --ignore @tenlastic/*-ui --ignore @tenlastic/ng-* --scope @tenlastic/* --scope nodejs build
-lerna run --ci --ignore @tenlastic/*-ui --ignore @tenlastic/ng-* --scope @tenlastic/* --scope nodejs test
+lerna bootstrap --ci --include-dependencies --ignore @tenlastic/*-ui --ignore @tenlastic/ng-* --scope @tenlastic/* --scope nodejs --since
+lerna run --ci --ignore @tenlastic/*-ui --ignore @tenlastic/ng-* --scope @tenlastic/* lint
+lerna run --ci --ignore @tenlastic/*-ui --ignore @tenlastic/ng-* --scope @tenlastic/* build
+lerna run --ci --ignore @tenlastic/*-ui --ignore @tenlastic/ng-* --scope @tenlastic/* test
 
 # Remove Node Modules.
 find . -name "node_modules" -exec rm -rf '{}' +
