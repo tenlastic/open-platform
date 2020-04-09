886ebbf9f2ee507a408fd66e9c9abe539d350a46
appellod <appellod@gmail.com>
2020-04-09 09:03:14 -0400

    remove q flag from docker push

--- .argo-ci/publish-docker-images.sh
+++ .argo-ci/publish-docker-images.sh
@@ -12,5 +12,5 @@ mv /tmp/docker/* /usr/bin
 # Build and Push Docker Images.
 cd "${ROOT}/projects/javascript/"
 npm i -g lerna
-lerna run docker:build --ci --concurrency 1
+lerna run docker:build --ci
 lerna run docker:push --ci --concurrency 1
