576b685165e30f048a2d54901e191fd0bf1011b5
appellod <appellod@gmail.com>
2020-04-10 04:34:11 -0400

    upgrade gke cluster version

--- gcp/terraform/cluster/cluster.tf
+++ gcp/terraform/cluster/cluster.tf
@@ -1,9 +1,9 @@
 resource "google_container_cluster" "primary" {
   initial_node_count       = 1
   location                 = "${var.zone}"
-  min_master_version       = "1.14.8-gke.17"
+  min_master_version       = "1.14.10-gke.27"
   name                     = "primary"
-  node_version             = "1.14.8-gke.17"
+  node_version             = "1.14.10-gke.27"
   remove_default_node_pool = true
 
   ip_allocation_policy {
