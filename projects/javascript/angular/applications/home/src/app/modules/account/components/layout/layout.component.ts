0b90c5af50e0f391593f6a3e4554785c84cdbc72
appellod <appellod@gmail.com>
2020-08-29 13:27:52 -0400

    set launcherUrl in account information

--- projects/javascript/angular/applications/home/src/app/modules/account/components/layout/layout.component.ts
+++ projects/javascript/angular/applications/home/src/app/modules/account/components/layout/layout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { environment } from '../../../../../environments/environment';
 import { ElectronService } from '../../../../core/services';
 
 @Component({
@@ -7,5 +8,7 @@ import { ElectronService } from '../../../../core/services';
   templateUrl: './layout.component.html',
 })
 export class LayoutComponent {
+  public launcherUrl = environment.launcherUrl;
+
   constructor(public electronService: ElectronService) {}
 }
