5e8c031cf95f9fee5115cb155bee997c73d91ca0
appellod <appellod@gmail.com>
2020-12-02 13:44:07 -0500

    fix migrations

--- .argo-ci/migrations.sh
+++ .argo-ci/migrations.sh
@@ -4,5 +4,7 @@ set -e
 ROOT=$(pwd)
 
 # Run Javascript Applications.
-cd "${ROOT}/projects/javascript/applications/migrations/"
+cd "${ROOT}/projects/javascript/nodejs/applications/migrations/"
+npm i -g lerna
+lerna bootstrap --ci --include-dependencies --scope @tenlastic/migrations --scope nodejs
 npm run start
