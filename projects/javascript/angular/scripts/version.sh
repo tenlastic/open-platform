a12e6810a66df3525a83470b021c31fe019e16f2
appellod <appellod@gmail.com>
2021-04-21 18:57:23 -0400

    fix angular version script

--- projects/javascript/angular/scripts/version.sh
+++ projects/javascript/angular/scripts/version.sh
@@ -2,7 +2,7 @@
 set -e
 
 DIRECTORY="${1}"
-FILE="../../../../../kubernetes/base/angular/${DIRECTORY}/deployment.yaml"
+FILE="../../../../../kubernetes/base/angular/${DIRECTORY}.yaml"
 VERSION=$(node -p "require('./package.json').version")
 
 # Bump tag to the most recent version.
