bb26708e509bdcbfab530729947769fe04160430
appellod <appellod@gmail.com>
2021-04-24 03:32:42 -0400

    remove kafka from database tests

--- projects/javascript/nodejs/applications/database/src/index.spec.ts
+++ projects/javascript/nodejs/applications/database/src/index.spec.ts
@@ -1,9 +1,6 @@
-import * as kafka from '@tenlastic/kafka';
 import * as mongooseModels from '@tenlastic/mongoose-models';
 
 before(async function() {
-  await kafka.connect(process.env.KAFKA_CONNECTION_STRING);
-
   await mongooseModels.connect({
     connectionString: process.env.MONGO_CONNECTION_STRING,
     databaseName: `database-test`,
