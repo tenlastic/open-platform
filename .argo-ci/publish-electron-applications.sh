f970fd98e8294210e67ad044ab3ed6e550251c82
appellod <appellod@gmail.com>
2021-04-26 07:02:16 -0400

    update workflow

--- .argo-ci/publish-electron-applications.sh
+++ .argo-ci/publish-electron-applications.sh
@@ -5,5 +5,6 @@ ROOT=$(pwd)
 cd "${ROOT}/projects/javascript/"
 
 # Publish Electron applications.
+npm i -g lerna
 lerna run electron:install --ci
 lerna run electron:build --ci -- -- --publish always --win
