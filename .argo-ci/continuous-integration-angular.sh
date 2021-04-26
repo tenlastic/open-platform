f8986073bccf057ec0ce158c76c25d3371626098
appellod <appellod@gmail.com>
2021-04-26 03:59:40 -0400

    build angular before testing

--- .argo-ci/continuous-integration-angular.sh
+++ .argo-ci/continuous-integration-angular.sh
@@ -6,5 +6,5 @@ cd "${ROOT}/projects/javascript/"
 
 # Lint, test, and build Angular applications.
 lerna run lint --ci --scope @tenlastic/*-ui --scope @tenlastic/ng-* --since
-lerna run test --ci --concurrency 1 --scope @tenlastic/*-ui --scope @tenlastic/ng-* --since
 lerna run build --ci --concurrency 1 --include-dependencies --scope @tenlastic/*-ui --scope @tenlastic/ng-* --since
+lerna run test --ci --concurrency 1 --scope @tenlastic/*-ui --scope @tenlastic/ng-* --since
