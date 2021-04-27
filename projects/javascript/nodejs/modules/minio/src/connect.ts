81f4e2bce07b95ba6320f0c27abbf3c8a7fcb087
appellod <appellod@gmail.com>
2021-04-26 23:25:50 -0400

    decode URI within minio module

--- projects/javascript/nodejs/modules/minio/src/connect.ts
+++ projects/javascript/nodejs/modules/minio/src/connect.ts
@@ -3,6 +3,10 @@ import * as Minio from 'minio';
 export let client: Minio.Client;
 
 export function connect(options: Minio.ClientOptions) {
+  options.accessKey = decodeURIComponent(options.accessKey);
+  options.endPoint = decodeURIComponent(options.endPoint);
+  options.secretKey = decodeURIComponent(options.secretKey);
+
   client = new Minio.Client(options);
 }
 
