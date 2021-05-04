5f6afab4b8ab45ceeecb3e34f2ef8bd1f065fdf0
appellod <appellod@gmail.com>
2021-05-04 19:52:25 -0400

    use bitnami mongodb instead of custom replica set image

--- projects/javascript/nodejs/modules/mongoose-change-stream-kafka/src/index.spec.ts
+++ projects/javascript/nodejs/modules/mongoose-change-stream-kafka/src/index.spec.ts
@@ -1,9 +1,6 @@
-import * as kafka from '@tenlastic/kafka';
 import * as mongoose from 'mongoose';
 
 before(async function() {
-  await kafka.connect(process.env.KAFKA_CONNECTION_STRING);
-
   await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
     dbName: 'mongoose-change-stream-kafka-test',
     useCreateIndex: true,
