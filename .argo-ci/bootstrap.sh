66776825e000f4514c704f40edae0c441e21bbed
appellod <appellod@gmail.com>
2021-06-12 12:59:21 -0400

    remove ci-node-modules

--- .argo-ci/bootstrap.sh
+++ .argo-ci/bootstrap.sh
@@ -4,11 +4,5 @@ set -e
 ROOT=$(pwd)
 cd "${ROOT}/projects/javascript/"
 
-# Install RSync.
-apt-get update
-apt-get install -qy rsync
-
 # Install NPM dependencies.
-rsync -a /ci-node-modules/ ./node_modules/
 lerna bootstrap --ci
-rsync -a --delete ./node_modules/ /ci-node-modules/
