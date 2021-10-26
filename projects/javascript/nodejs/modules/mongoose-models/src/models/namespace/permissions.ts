83ffeaa38be159c83b116039394b9d172568f5a5
appellod <appellod@gmail.com>
2021-10-26 00:03:30 -0400

    add system administrator to namespace permissions

--- projects/javascript/nodejs/modules/mongoose-models/src/models/namespace/permissions.ts
+++ projects/javascript/nodejs/modules/mongoose-models/src/models/namespace/permissions.ts
@@ -3,73 +3,6 @@ import { MongoosePermissions } from '@tenlastic/mongoose-permissions';
 import { UserPermissionsHelpers, UserRole } from '../user';
 import { Namespace, NamespaceDocument, NamespaceRole } from './model';
 
-export const NamespacePermissions = new MongoosePermissions<NamespaceDocument>(Namespace, {
-  create: {
-    'user-administrator': ['keys.*', 'limits.*', 'name', 'users.*'],
-  },
-  delete: {
-    default: false,
-    'user-administrator': true,
-  },
-  find: {
-    default: {
-      $or: [
-        { keys: { $elemMatch: { roles: NamespaceRole.Namespaces, value: { $ref: 'key' } } } },
-        { users: { $elemMatch: { _id: { $ref: 'user._id' }, roles: NamespaceRole.Namespaces } } },
-      ],
-    },
-    'user-administrator': {},
-  },
-  read: {
-    default: ['_id', 'createdAt', 'name', 'updatedAt'],
-    'namespace-administrator': [
-      '_id',
-      'createdAt',
-      'keys.*',
-      'limits.*',
-      'name',
-      'updatedAt',
-      'users.*',
-    ],
-    'user-administrator': [
-      '_id',
-      'createdAt',
-      'keys.*',
-      'limits.*',
-      'name',
-      'updatedAt',
-      'users.*',
-    ],
-  },
-  roles: [
-    {
-      name: 'user-administrator',
-      query: UserPermissionsHelpers.getRoleQuery(UserRole.Namespaces),
-    },
-    {
-      name: 'namespace-administrator',
-      query: {
-        $or: [
-          {
-            'record.keys': {
-              $elemMatch: { roles: NamespaceRole.Namespaces, value: { $ref: 'key' } },
-            },
-          },
-          {
-            'record.users': {
-              $elemMatch: { _id: { $ref: 'user._id' }, roles: NamespaceRole.Namespaces },
-            },
-          },
-        ],
-      },
-    },
-  ],
-  update: {
-    'namespace-administrator': ['keys.*', 'name', 'users.*'],
-    'user-administrator': ['keys.*', 'limits.*', 'name', 'users.*'],
-  },
-});
-
 export const NamespacePermissionsHelpers = {
   getFindQuery(role: NamespaceRole) {
     return {
@@ -107,3 +40,71 @@ export const NamespacePermissionsHelpers = {
     };
   },
 };
+
+export const NamespacePermissions = new MongoosePermissions<NamespaceDocument>(Namespace, {
+  create: {
+    'user-administrator': ['keys.*', 'limits.*', 'name', 'users.*'],
+  },
+  delete: {
+    default: false,
+    'user-administrator': true,
+  },
+  find: {
+    default: {
+      $or: [
+        { keys: { $elemMatch: { roles: NamespaceRole.Namespaces, value: { $ref: 'key' } } } },
+        { users: { $elemMatch: { _id: { $ref: 'user._id' }, roles: NamespaceRole.Namespaces } } },
+        NamespacePermissionsHelpers.getNamespaceUserFindQuery(NamespaceRole.Databases),
+      ],
+    },
+    'user-administrator': {},
+  },
+  read: {
+    default: ['_id', 'createdAt', 'name', 'updatedAt'],
+    'namespace-administrator': [
+      '_id',
+      'createdAt',
+      'keys.*',
+      'limits.*',
+      'name',
+      'updatedAt',
+      'users.*',
+    ],
+    'system-administrator': [
+      '_id',
+      'createdAt',
+      'keys.*',
+      'limits.*',
+      'name',
+      'updatedAt',
+      'users.*',
+    ],
+    'user-administrator': [
+      '_id',
+      'createdAt',
+      'keys.*',
+      'limits.*',
+      'name',
+      'updatedAt',
+      'users.*',
+    ],
+  },
+  roles: [
+    {
+      name: 'system-administrator',
+      query: NamespacePermissionsHelpers.getNamespaceUserRoleQuery(NamespaceRole.Namespaces),
+    },
+    {
+      name: 'user-administrator',
+      query: UserPermissionsHelpers.getRoleQuery(UserRole.Namespaces),
+    },
+    {
+      name: 'namespace-administrator',
+      query: NamespacePermissionsHelpers.getRoleQuery(NamespaceRole.Namespaces, 'record'),
+    },
+  ],
+  update: {
+    'namespace-administrator': ['keys.*', 'name', 'users.*'],
+    'user-administrator': ['keys.*', 'limits.*', 'name', 'users.*'],
+  },
+});
