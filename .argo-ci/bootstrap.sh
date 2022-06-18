8bfab11a28d7aa16c07de27c144ba92e17a65a99
appellod <appellod@gmail.com>
2022-06-18 18:34:42 -0400

    fix bootstrap

--- .argo-ci/bootstrap.sh
+++ .argo-ci/bootstrap.sh
@@ -5,19 +5,4 @@ ROOT=$(pwd)
 cd "${ROOT}/projects/javascript/"
 
 # Install NPM dependencies.
-time lerna \
-  --scope @tenlastic/*-ui \
-  --scope @tenlastic/ng-* \
-  --scope angular \
-  bootstrap \
-  --ci \
-  --concurrency 1 \
-  --registry "https://verdaccio.tenlastic.com"
-time lerna \
-  --ignore @tenlastic/*-ui \
-  --ignore @tenlastic/ng-* \
-  --scope @tenlastic/* \
-  --scope nodejs \
-  bootstrap \
-  --ci \
-  --registry "https://verdaccio.tenlastic.com"
+lerna bootstrap --ci --registry "https://verdaccio.tenlastic.com"
