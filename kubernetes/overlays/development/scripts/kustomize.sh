880cf681354cf07de706c0bedbf6ff11e1d2b5e2
appellod <appellod@gmail.com>
2022-06-19 17:38:20 -0400

    fix angular to work locally

--- kubernetes/overlays/development/scripts/kustomize.sh
+++ kubernetes/overlays/development/scripts/kustomize.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Create remaining resources.
-kustomize build ./ | kubectl delete -f -
+kustomize build ./ | kubectl apply -f -
 
 # Apply local, uncommitted secret files. (Kubectl PR: https://github.com/kubernetes/kubernetes/pull/102265)
-#kubectl apply $(find ../local/ -name '*.secret.yaml' -type f | awk ' { print " -f " $1 } ')
-#kubectl apply $(find ./ -name '*.secret.yaml' -type f | awk ' { print " -f " $1 } ')
+kubectl apply $(find ../local/ -name '*.secret.yaml' -type f | awk ' { print " -f " $1 } ')
+kubectl apply $(find ./ -name '*.secret.yaml' -type f | awk ' { print " -f " $1 } ')
