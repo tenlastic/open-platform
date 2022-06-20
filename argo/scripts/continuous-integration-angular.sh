7f406b79c8130581e77b18d0bc5efb6b4a9a52c9
appellod <appellod@gmail.com>
2022-06-20 07:06:41 -0400

    specify --since parameter in ci scripts

--- argo/scripts/continuous-integration-angular.sh
+++ argo/scripts/continuous-integration-angular.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
+TAG=$(git describe --match "angular-v*" --abbrev=0 HEAD)
+REVISION=$(git log -1 --format=format:"%H" $TAG)
+
 # Lint, test, and build Angular applications.
-lerna run lint --since
-lerna run build --concurrency 1 --include-dependencies --since
-lerna run test --concurrency 1 --since
+lerna run lint --since $REVISION
+lerna run build --concurrency 1 --include-dependencies --since $REVISION
+lerna run test --concurrency 1 --since $REVISION
--- argo/scripts/continuous-integration-nodejs.sh
+++ argo/scripts/continuous-integration-nodejs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
+TAG=$(git describe --match "nodejs-v*" --abbrev=0 HEAD)
+REVISION=$(git log -1 --format=format:"%H" $TAG)
+
 # Lint, build, and test NodeJS applications.
-lerna run lint --since 
-lerna run build --include-dependencies --since
-lerna run test --since
+lerna run lint --since $REVISION
+lerna run build --include-dependencies --since $REVISION
+lerna run test --since $REVISION
