5b5aa63919f5bebea79fe6b7798a28a80a72fd1b
appellod <appellod@gmail.com>
2022-07-22 00:45:42 -0400

    export substitute reference values

--- nodejs/modules/mongoose-permissions/src/index.ts
+++ nodejs/modules/mongoose-permissions/src/index.ts
@@ -2,4 +2,4 @@ export * from './access-control';
 export * from './filter-object';
 export * from './is-json-valid';
 export * from './mongoose-permissions';
-export * from './substitute-subquery-values';
+export * from './substitute-reference-values';
