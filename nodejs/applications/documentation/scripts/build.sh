04b3dfa60808784c0a324eb33851a0e728efb037
appellod <appellod@gmail.com>
2022-11-27 14:03:55 -0500

    update documentation

--- nodejs/applications/documentation/scripts/build.sh
+++ nodejs/applications/documentation/scripts/build.sh
@@ -7,10 +7,11 @@ cp -r ./src/. ./dist/
 # Merge swagger files.
 merge-yaml \
   -i ./swagger.yml \
-  ../api/src/mongodb/**/swagger.yml \
-  ../api/src/web-server/**/swagger.yml \
-  ../namespace-api/src/mongodb/**/swagger.yml \
-  ../namespace-api/src/web-server/**/swagger.yml \
+  ../../modules/mongoose/src/**/*.yml \
+  ../aggregation-api/src/**/swagger.yml \
+  ../api/src/**/swagger.yml \
+  ../namespace-api/src/**/swagger.yml \
+  ../social-api/src/**/swagger.yml \
   -o ./dist/swagger.yml
 
 # Alphabetize Output.
