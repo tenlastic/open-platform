5c5d6c16bade631a897fe3bc9573bae5e723c608
appellod <appellod@gmail.com>
2023-01-03 07:15:26 -0500

    use port 80 for documentation in development

--- nodejs/applications/documentation/scripts/start.sh
+++ nodejs/applications/documentation/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-onchange -i -k -p 1000 "./src/**" -- bash -c "./scripts/build.sh && http-server --cors '*' -p 3000 ./dist/"
+onchange -i -k -p 1000 "./src/**" -- bash -c "./scripts/build.sh && http-server --cors '*' -p 80 ./dist/"
