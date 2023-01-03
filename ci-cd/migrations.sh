ccb661cbf6144128570f73835d73262b77d4d1fc
appellod <appellod@gmail.com>
2023-01-03 04:53:17 -0500

    include social api in migrations

--- ci-cd/migrations.sh
+++ ci-cd/migrations.sh
@@ -6,8 +6,10 @@ lerna run \
   --include-dependencies \
   --scope @tenlastic/aggregation-api-migrations \
   --scope @tenlastic/api-migrations \
+  --scope @tenlastic/social-api-migrations \
   build
 lerna run \
   --scope @tenlastic/aggregation-api-migrations \
   --scope @tenlastic/api-migrations \
+  --scope @tenlastic/social-api-migrations \
   start
