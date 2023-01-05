ae2a0862f42a320183d2bd4e15d77778ffe1650c
appellod <appellod@gmail.com>
2023-01-05 10:06:08 -0500

    bump nodejs to 0.0.338

--- ci-cd/publish-npm-modules.sh
+++ ci-cd/publish-npm-modules.sh
@@ -23,14 +23,14 @@ git push --atomic --follow-tags --no-verify origin master
 
 # Publish Node Modules to NPM.
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
-lerna publish --yes from-package
+lerna publish --concurrency 1 --yes from-package
 
 # Publish Node Modules to Github.
 echo "@tenlastic:registry=https://npm.pkg.github.com" > ~/.npmrc
 echo "//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}" >> ~/.npmrc
-lerna publish --registry "https://npm.pkg.github.com" --yes from-package
+lerna publish --concurrency 1 --registry "https://npm.pkg.github.com" --yes from-package
 
 # Publish Node Modules to Verdaccio.
 echo "@tenlastic:registry=https://verdaccio.tenlastic.com" > ~/.npmrc
 echo "//verdaccio.tenlastic.com/:_authToken=${VERDACCIO_TOKEN}" >> ~/.npmrc
-lerna publish --registry "https://verdaccio.tenlastic.com" --yes from-package
+lerna publish --concurrency 1 --registry "https://verdaccio.tenlastic.com" --yes from-package
