27653e1fba50eddf6bce77b91c578cf716f1d248
appellod <appellod@gmail.com>
2023-01-09 08:06:21 -0500

    add logging to syncIndexes

--- nodejs/modules/mongoose/src/sync-indexes.ts
+++ nodejs/modules/mongoose/src/sync-indexes.ts
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 
 export function syncIndexes(Model: mongoose.Model<mongoose.Document>) {
-  return Model.syncIndexes({ background: true });
+  console.log(`Connection ready state: ${Model.db.readyState}.`);
+  console.log(`Syncing indexes for ${Model.modelName}...`);
+
+  const result = Model.syncIndexes({ background: true });
+
+  console.log(`Synced indexes for ${Model.modelName} successfully.`);
+
+  return result;
 }
