dba9e332f412b28fb0ab1a8e606e08583150fc08
appellod <appellod@gmail.com>
2023-01-19 11:26:31 -0500

    add more logging to password resets

--- nodejs/applications/api/src/nats/password-reset.ts
+++ nodejs/applications/api/src/nats/password-reset.ts
@@ -7,7 +7,10 @@ import mailgun from '../mailgun';
 PasswordResetEvent.async(async (payload) => {
   const { fullDocument } = payload;
 
+  console.log(`Password Reset Event.`);
+
   if (payload.operationType === 'insert') {
+    console.log(`Sending Password Reset Request...`);
     const user = await UserModel.findOne({ _id: fullDocument.userId });
     const response = await mailgun.sendPasswordResetRequest({
       email: user.email,
