0e91a95fb495d7eb4c2f657d8cb4a74236de0831
appellod <appellod@gmail.com>
2023-02-12 09:34:03 -0500

    add podSecurityContext to nats chart

--- kustomize/overlays/development/scripts/kustomize.sh
+++ kustomize/overlays/development/scripts/kustomize.sh
@@ -2,7 +2,11 @@
 set -e
 
 # Create remaining resources.
-kustomize build ./ | kubectl apply -f -
+until kustomize build ./ | kubectl apply -f -
+do
+  echo "Error running Kustomize. Retrying in 5 seconds..."
+  sleep 5
+done
 
 # Apply local, uncommitted secret files. (Kubectl PR: https://github.com/kubernetes/kubernetes/pull/102265)
 kubectl apply $(find ../../base/ -name '*.secret.yaml' -type f | awk ' { print " -f " $1 } ')
