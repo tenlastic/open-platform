de343ad91ad6b87b7fbdf9dc1c10068b70ffabcb
appellod <appellod@gmail.com>
2023-03-13 15:08:01 -0400

    fix publish electron applications

--- scripts/publish-electron-applications.sh
+++ scripts/publish-electron-applications.sh
@@ -4,4 +4,4 @@ set -e
 # Publish Electron applications.
 npm i -g lerna
 lerna run electron:install
-lerna run electron:build -- -- --publish always --win
+lerna run electron:build -- --publish always --win
