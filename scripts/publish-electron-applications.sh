f6378f05af94e39f404d4694a2986d77d42ca8f8
Dan Appello <appellod@gmail.com>
2025-03-22 05:06:08 -0400

    fix: try splitting up electron commands

--- scripts/publish-electron-applications.sh
+++ scripts/publish-electron-applications.sh
@@ -3,5 +3,7 @@ set -e
 
 # Publish Electron applications.
 npm i -g lerna@5.1.4
+lerna run build:electron
 lerna run electron:install
+lerna run electron:tsc
 lerna run electron:build -- --publish always --win
